9857a004c85c4913289e171303d11c17ac58571e
jinimbrancalhao <jinjinim717@gmail.comgit config --global user.email jinjinim717@gmail.com>
2021-08-30 17:12:14 -0400

    get controller

--- controllers/index.js
+++ controllers/index.js
@@ -12,6 +12,16 @@ const createReflecton = async (req, res) => {
   }
 }
 
+const getAllReflections = async (req, res) => {
+  try {
+    const reflection = await Reflection.find()
+    return res.status(200).json({ reflections })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
 module.exports = {
-  createReflecton
+  createReflecton,
+  getAllReflections
 }
