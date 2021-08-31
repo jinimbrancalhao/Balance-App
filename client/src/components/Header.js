ad3194a4d4e7a4754fb654b904878c6dcb836c76
jinimbrancalhao <jinjinim717@gmail.comgit config --global user.email jinjinim717@gmail.com>
2021-08-31 11:49:18 -0400

    add nav links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,15 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Header = () => {
-  return <div></div>
+  return (
+    <header>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/goals">Goals</NavLink>
+        <NavLink to="/reflections">Reflections</NavLink>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
