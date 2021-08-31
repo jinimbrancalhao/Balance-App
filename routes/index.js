b4a90e76b737fb651413ce5129511f29b807a95e
jinimbrancalhao <jinjinim717@gmail.comgit config --global user.email jinjinim717@gmail.com>
2021-08-30 23:01:42 -0400

    routes goals

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 const { Router } = require('express')
-const controllers = require('../controllers/reflectionControl')
+const reflections = require('../controllers/reflectionControl')
+const goals = require('../controllers/goalControl')
 const router = Router()
 
 router.get('/', (req, res) => res.send('This is root!'))
 
-router.post('/reflections', controllers.createReflection)
+router.post('/reflections', reflections.createReflection)
 
-router.get('/reflections', controllers.getAllReflections)
+router.get('/reflections', reflections.getAllReflections)
 
-router.delete('/reflections/:id', controllers.deleteReflection)
+router.delete('/reflections/:id', reflections.deleteReflection)
+
+router.post('/goals', goals.createGoal)
+
+router.get('/goals', goals.getAllGoals)
+
+router.delete('/goals/:id', goals.deleteGoal)
 
 module.exports = router
