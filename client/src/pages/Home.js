5046b23d27365be3ca34dcc1babb82437a942272
jinimbrancalhao <jinjinim717@gmail.comgit config --global user.email jinjinim717@gmail.com>
2021-08-31 20:24:01 -0400

    add submit frame

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,26 @@
 import React from 'react'
 
 const Home = () => {
+  const handleChange = () => {}
+
+  const handleSubmit = () => {}
+
   return (
     <div>
       <div>Make Goal</div>
-      <form>
-        <input name="title" />
-        <input name="start" />
-        <input name="end" />
-        <input name="description" />
+      <form onSubmit={handleSubmit}>
+        <input name="title" onChange={handleChange} />
+        <input name="start" onChange={handleChange} />
+        <input name="end" onChange={handleChange} />
+        <input name="description" onChange={handleChange} />
+        <button type="submit">Submit</button>
       </form>
       <div>Make Reflection</div>
-      <form>
-        <input name="title" />
-        <input name="date" />
-        <input name="description" />
+      <form onSubmit={handleSubmit}>
+        <input name="title" onChange={handleChange} />
+        <input name="date" onChange={handleChange} />
+        <input name="description" onChange={handleChange} />
+        <button type="submit">Submit</button>
       </form>
     </div>
   )
